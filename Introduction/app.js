ded6fd8ced0fde64888a35053069b855bc64cf0b
Krittin Chaowakarn <130139478+krittin-ch@users.noreply.github.com>
2024-02-27 20:42:07 +0700

    Update app.js

--- Introduction/app.js
+++ Introduction/app.js
@@ -1,10 +1,23 @@
 const http = require('http')
 
-const server = http.createServer((req,res) => { 
-    console.log('request event') // run when create server (click at reload button)
-    res.end('Hello World')
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        res.end('Home Page')
+        return
+    }
+    if (req.url === '/about') {
+        // BLOCKING CODE !!! (with this synchronous setup, the browser is loading until the for loop end)
+        for (let i=0; i<1000; i++) {
+            for (let j=0; j<1000; j++) {
+                console.log(`${i} ${j}`)
+            }
+        }
+        res.end('About Page')
+        return
+    }
+    res.end('Error Page')
 })
 
-server.listen(3000, () => {
-    console.log('Server listening on port : 3000.....')
+server.listen(5000, () => {
+    console.log('Server is listening on port 5000...')
 })
