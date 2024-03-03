a63ef620190ef28ba7e0f2002cf5b61c861e018f
Krittin Chaowakarn <130139478+krittin-ch@users.noreply.github.com>
2024-03-03 14:56:11 +0700

    Update app.js

--- Introduction/app.js
+++ Introduction/app.js
@@ -1,23 +1,18 @@
-const http = require('http')
+const {readFile} = require('fs')
+const { result } = require('lodash')
 
-const server = http.createServer((req, res) => {
-    if (req.url === '/') {
-        res.end('Home Page')
-        return
-    }
-    if (req.url === '/about') {
-        // BLOCKING CODE !!! (with this synchronous setup, the browser is loading until the for loop end)
-        for (let i=0; i<1000; i++) {
-            for (let j=0; j<1000; j++) {
-                console.log(`${i} ${j}`)
+const getText = (path) => {
+    return new Promise((resolve, reject) => {
+        readFile(path,'utf-8', (err, data) => {
+            if (err) {
+                reject(err)
+            } else {
+                resolve(data)
             }
-        }
-        res.end('About Page')
-        return
-    }
-    res.end('Error Page')
-})
+        })
+    })
+}
 
-server.listen(5000, () => {
-    console.log('Server is listening on port 5000...')
-})
+getText('./Introduction/content/fidrst.txt')
+.then(result => console.log(result))
+.catch((err) => console.log(err))
