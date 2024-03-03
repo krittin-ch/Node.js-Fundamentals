294f631b79c38da34cdb04f6429eb76735192127
Krittin Chaowakarn <130139478+krittin-ch@users.noreply.github.com>
2024-03-03 14:56:22 +0700

    Delete

--- Introduction/13b-event.js
+++
@@ -1,9 +0,0 @@
-// second event loop
-// starting operating system process
-console.log('first')
-setTimeout(() => {
-    console.log('second')
-}, 0)
-
-console.log('third')
-// completed and exited operating system process
